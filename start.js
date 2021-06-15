bf759d48312728b8e90e810440fea7a3630ff0c1
TFTWPhoenix <tftwphoenix@missingblox.com>
2021-06-15 00:01:22 -0500

    Directory Mode; 404 Page; Default Index.html

--- start.js
+++ start.js
@@ -1,12 +1,25 @@
 // Requires
 const http = require('http');
 const url = require('url');
+const mainconfig = require('./config/main.json');
+const fs = require('fs');
 
 // Create the server.
 http.createServer(function (req, res) {
     let queryObject = url.parse(req.url,true).query; //  Query Object
     let path = url.parse(req.url,true).path.split("?")[0];
-  
+    
     res.writeHead(200, {'Content-Type': 'text/html'}); // We're sending HTML data here.
-    res.end(`Path: ${path}<br/>Query: ${JSON.stringify(queryObject)}`);
+
+    if(mainconfig.servermode == "directory") {
+        if(path == "/") {
+            res.end(fs.readFileSync("www/index.html"));
+        } else if (fs.readdirSync("www").includes(path.substr(1,path.length))) {
+            res.end(fs.readFileSync("www" + path));
+        } else {
+            res.end(fs.readFileSync("internal/404.html"));
+        }
+    } else {
+        res.end('A supported mode was not chosen.');
+    }
   }).listen(8080); // Listen on PORT 8080.
